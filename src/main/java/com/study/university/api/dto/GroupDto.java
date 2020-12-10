460c4afbde4f417c90bbe484e4214bea4a8cfdea
verenko <Verenko2000@gmail.com>
2020-12-10 17:44:03 +0200

    add userDto

--- src/main/java/com/study/university/api/dto/GroupDto.java
+++ src/main/java/com/study/university/api/dto/GroupDto.java
@@ -1,4 +1,26 @@
 package com.study.university.api.dto;
 
+import com.study.university.api.model.Group;
+import com.study.university.api.model.Status;
+import com.study.university.api.model.Student;
+
+import java.util.Date;
+
 public class GroupDto {
+    private Long id;
+    private String name;
+
+
+    public Group toGroup() {
+        Group group = new Group();
+        group.setId(id);
+        group.setName(name);
+        return group;
+    }
+
+    public static GroupDto fromGroup(Group group) {
+        GroupDto groupDto = new GroupDto();
+        group.setName(group.getName());
+        return groupDto;
+    }
 }
